1865f9c1c692d47bee1ab0f6d62b3f19b3ea8367
Daniel F. Dickinson <cshored@thecshore.com>
2017-12-31 16:18:45 -0500

    gulp modules: Use node modules where possible rather than git submodules
    And add the new asset pipeline without the race
    condition problem.
    
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,83 +3,47 @@ var babel = require("gulp-babel");
 var babel_register = require('babel-register');
 var concat = require("gulp-concat");
 var sourcemaps = require("gulp-sourcemaps");
-var require_dir = require("require-dir");
 var fs = require("graceful-fs");
-var process = require("process");
-import { execSync } from 'child_process';
-var runSequence = require('run-sequence');
 var rename = require('gulp-rename');
-var glob = require('glob');
+var yarn = require('gulp-yarn');
+var evstr = require('event-stream');
 
-gulp.task("build_modules", function(done) {
-  var moduleDirs;
-  var moduleDir;
-  var err = null;
-  var err2 = null;
-  var olddir;
-  var files;
-  moduleDirs = glob.sync("./modules/yahoo-pure");
-  if (moduleDirs) {
-    var errOut = moduleDirs.forEach(function(moduleDir, index) {
-      olddir = process.cwd();
-      try {
-        process.chdir(moduleDir);
-        files = glob.sync('package.json');
-        if (typeof files!= "undefined" && files != null && files.length > 0) {
-          try {
-            execSync("yarn install");
-          } catch (err) {
-          }
-          files = glob.sync('gulpfile*.js');
-          if (typeof files!= "undefined" && files != null && files.length > 0) {
-            errOut = execSync("gulp");
-          } else {
-            files = glob.sync('Gruntfile*.js');
-            if (typeof files!= "undefined" && files != null && files.length > 0) {
-              errOut = execSync("grunt");
-            } else {
-              errOut = "Neither gulpfile*.js not Gruntfile.js exist";
-            }
-            err = errOut;
-          }
-        } else {
-          console.log("Didn't find package.json");
-        }
-        if (err) {
-          err2 = err.toString();
-        }
-      } catch (err) {
-        err2= err.toString();
-      }
-      process.chdir(olddir);
-      if (err2) {
-        return err2;
-      } else {
-        return null;
-      }
-    });
-    done(errOut);
-  } else {
-    done("No submodules found.");
-  }
-});
 
-gulp.task("js", function () {
-  runSequence
-  return gulp.src(["src/**/*.js","modules/*/src/*.js","modules/*/modules/*/src/*.js","!**/*.min.js","!**/*-min.js"])
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(concat("all.js"))
-    .pipe(gulp.dest("static"))
-    .pipe(babel({ "presets": ['minify'] }))
-    .pipe(rename({extname: '-min.js'}))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("static"));
-});
-
-gulp.task("default", function () {
-  runSequence(
-    'build_modules',
-    'js'
+gulp.task("yarn", function(done) {
+  return evstr.concat(
+    gulp.src(['./modules/package.json'])
+      .pipe(gulp.dest('./modules'))
+      .pipe(yarn({
+        production: true,
+        flat: true,
+        noBinLinks: true,
+        ignoreScripts: true,
+        nonInteractive: true
+    })),
+    gulp.src(['./local-modules/cshore-styling-tools/modules/package.json'])
+      .pipe(gulp.dest('./local-modules/cshore-styling-tools/modules'))
+      .pipe(yarn({
+        production: true,
+        flat: true,
+        noBinLinks: true,
+        ignoreScripts: true,
+        nonInteractive: true
+    }))
   );
 });
+
+gulp.task("js", ['yarn'], function (done) {
+  return evstr.concat(
+    gulp.src(["src/**/*.js","local-modules/src/**/*.js","local-modules/node_modules/*/src/**/*.js","!**/*.min.js","!**/*-min.js"])
+     .pipe(sourcemaps.init())
+     .pipe(babel())
+     .pipe(concat("all.js"))
+     .pipe(gulp.dest("static"))
+     .pipe(babel({ "presets": ['minify'] }))
+     .pipe(rename({extname: '-min.js'}))
+     .pipe(sourcemaps.write("."))
+     .pipe(gulp.dest("static"))
+  )
+});
+
+gulp.task("default", ['js','css']);
