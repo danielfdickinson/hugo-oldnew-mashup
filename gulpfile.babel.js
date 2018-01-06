a9148cfb515c63cf01289412a34b44806c8b2add
Daniel F. Dickinson <cshored@thecshore.com>
2018-01-06 07:26:46 -0500

    Move cshore-styling-tools into the theme as they're fairly specific to my theme/site
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,26 @@
+'use strict';
+
 var gulp = require("gulp");
-var babel = require("gulp-babel");
 var babel_register = require('babel-register');
+var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 var sourcemaps = require("gulp-sourcemaps");
-var fs = require("graceful-fs");
 var rename = require('gulp-rename');
+var fs = require('graceful-fs');
 var yarn = require('gulp-yarn');
 var evstr = require('event-stream');
+var tar = require('gulp-tar');
+var gzip = require('gulp-gzip');
+var zip = require('gulp-zip');
+var jshint= require('gulp-jshint');
+var eslint = require('gulp-eslint');
+var sass = require('gulp-sass');
+var csslint = require('gulp-csslint');
+var postcss = require('gulp-postcss');
+var cssImport = require('postcss-import');
+var cssnext = require('postcss-cssnext');
 
-
-gulp.task("yarn", function(done) {
+gulp.task('yarn', function (done) {
   return evstr.concat(
     gulp.src(['./modules/package.json'])
       .pipe(gulp.dest('./modules'))
@@ -18,77 +29,119 @@ gulp.task("yarn", function(done) {
         flat: true,
         noBinLinks: true,
         ignoreScripts: true,
-        nonInteractive: true
-    })),
-    gulp.src(['./local-modules/cshore-styling-tools/modules/package.json'])
-      .pipe(gulp.dest('./local-modules/cshore-styling-tools/modules'))
-      .pipe(yarn({
-        production: true,
-        flat: true,
-        noBinLinks: true,
-        ignoreScripts: true,
-        nonInteractive: true
-    }))
-  );
+        nonInteractive: true}))
+  )
 });
 
-gulp.task("js", ['yarn'], function (done) {
-  return evstr.concat(
-    gulp.src([
-      "local-modules/*/modules/node_modules/js-cookie/**/*.js",
-      "src/**/*.js",
-      "static-src/**/*.js",
-      "local-modules/*/src/**/*.js",
-      "!**/*.min.js",
-      "!**/*-min.js"
-     ])
-      .pipe(sourcemaps.init())
-      .pipe(babel())
-      .pipe(concat("all.js"))
-      .pipe(gulp.dest("dist/js"))
-      .pipe(babel({ "presets": ['minify'] }))
-      .pipe(rename({extname: '-min.js'}))
-      .pipe(sourcemaps.write("."))
-      .pipe(gulp.dest("dist/js"))
-  );
-});
-
-gulp.task("build", ["js"], function (done) {
+gulp.task('js', ['js-test'], function () {
   var pkg = JSON.parse(fs.readFileSync('./package.json'));
   return evstr.concat(
     gulp.src([
-      "static-src/**/*.js",
-      "dist/**/*.js",
-      "!dist/**/*.min.*",
-      "!dist/**/*-min.*"
-   ])
-    .pipe(gulp.dest('static/'))
-    .pipe(rename({basename: pkg.name + "-" + pkg.version}))
-    .pipe(gulp.dest('release/' + pkg.name + "-" + pkg.version + "/")),
-   gulp.src([
-      "dist/**/*.min.js",
-      "dist/**/*-min.js"
-   ])
-    .pipe(rename({basename: pkg.name + "-" + pkg.version + "-min"}))
-    .pipe(gulp.dest('release/' + pkg.name + "-" + pkg.version + "/")),
-    gulp.src([
-      "dist/**/*.css",
-      "local-modules/cshore-styling-tools/dist/**/*.css",
-      "static-src/**/*.css",
-      "!dist/**/*.min.css",
-      "!dist/**/*-min.css"
-   ])
-    .pipe(gulp.dest('static'))
-    .pipe(rename({basename: pkg.name + "-" + pkg.version}))
-    .pipe(gulp.dest('release/' + pkg.name + "-" + pkg.version + "/")),
-   gulp.src([
-      "dist/**/*.min.css",
-      "dist/**/*-min.css"
-   ])
-    .pipe(rename({basename: pkg.name + "-" + pkg.version + "-min"}))
-    .pipe(gulp.dest('release/' + pkg.name + "-" + pkg.version + "/"))
- );
+      "modules/node_modules/js-cookie/src/**/*.js",
+      "src/**/*.js",
+      "!**/*.min.js",
+      "!**/*-min.js"
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(gulp.dest("dist/separated/js"))
+    .pipe(concat(pkg.name + ".js"))
+    .pipe(gulp.dest("dist/js"))
+    .pipe(babel({ "presets": ['minify'] }))
+    .pipe(rename({extname: '-min.js'}))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist/js"))
+  );
 });
 
-gulp.task("default", ['build']);
-gulp.task("release", ['build']);
+gulp.task('release', ['js'], function() {
+  var pkg = JSON.parse(fs.readFileSync('./package.json'));
+  return evstr.concat(
+    gulp.src([
+      "dist/**/*.js",
+      "!dist/**/*-min.js",
+    ])
+   .pipe(rename({basename: pkg.name + "-" + pkg.version}))
+   .pipe(gulp.dest('release/' + pkg.name + '-' + pkg.version + '/')),
+  gulp.src([
+      "dist/**/*-min.*",
+    ])
+   .pipe(rename({basename: pkg.name + "-" + pkg.version + '-min'}))
+   .pipe(gulp.dest('release/' + pkg.name + '-' + pkg.version + '/')),
+  gulp.src([
+    "release/" + pkg.name + "-" + pkg.version + '/'
+  ])
+    .pipe(tar(pkg.name + "-" + pkg.version + '.tar'))
+    .pipe(gzip())
+    .pipe(gulp.dest('release')),
+  gulp.src([
+    "release/" + pkg.name + "-" + pkg.version + '/'
+  ])
+    .pipe(zip(pkg.name + "-" + pkg.version + '.zip'))
+  );
+});
+
+gulp.task('css', ['css-test'], function(done) {
+  var plugins = [
+     cssImport({path: ["src/imports/css"]}),
+     cssnext()
+  ]
+  return evstr.concat(
+    gulp.src([
+      "modules/node_modules/normalize.css/normalize.css",
+      "src/css/base/**/*.css",
+      "src/css/base-modern/**/*.css"
+   ])
+    .pipe(sourcemaps.init())
+    .pipe(postcss(plugins,{
+       browsers: ['>1%']
+     }))
+    .pipe(gulp.dest('dist/separated/css'))
+    .pipe(sourcemaps.write("."))
+    .pipe(concat("base.css"))
+    .pipe(gulp.dest("dist/css/"))
+    .pipe(sourcemaps.write("."))
+  );
+});
+
+gulp.task('js-test', function(done) {
+  return evstr.concat(
+    gulp.src([
+      "src/js/base/**/*.js"
+    ])
+      .pipe(babel())
+      .pipe(gulp.dest("build/lint/js"))
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+      .pipe(jshint.reporter('fail'))
+      .pipe(eslint({useElintrc: true, rules: {
+         'indent': 0
+       }}))
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError())
+  );
+});
+
+gulp.task('css-test', ['yarn'], function(done) {
+  var plugins = [
+     cssImport({path: ["src/imports/css"]}),
+     cssnext()
+  ]
+  return evstr.concat(
+    gulp.src([
+      "modules/node_modules/normalize.css/normalize.css",
+      "src/css/base/**/*.css",
+      "src/css/base-modern/**/*.css"
+   ])
+    .pipe(postcss(plugins,{
+       browsers: ['>1%']
+   }))
+    .pipe(gulp.dest("build/lint/css"))
+    .pipe(csslint())
+    .pipe(csslint(csslint.formatter('fail')))
+  );
+});
+
+gulp.task('test', ['js-test','css-test']);
+gulp.task('build', ['js','css']);
+gulp.task("default", ['test','build']);
