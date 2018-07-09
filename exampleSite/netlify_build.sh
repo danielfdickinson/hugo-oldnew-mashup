a653995f6a77833574626c25a9dc4d5e34becb82
Daniel F. Dickinson <cshored@thecshore.com>
2018-07-09 05:56:08 -0400

    exampleSite/netlify_buld: Fix paths because build already starts in exampleSite subdir
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

--- exampleSite/netlify_build.sh
+++ exampleSite/netlify_build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mkdir -p exampleSite/themes || exit 1
-ln -s ../.. exampleSite/themes/oldnew-mashup || exit 1
+mkdir -p themes || exit 1
+ln -s ../.. themes/oldnew-mashup || exit 1
+yarn install || exit 1
 gulp build && hugo || exit 1
