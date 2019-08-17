83c05c2bb2adbb24ebf0d767467fec7803a0c91f
Daniel F. Dickinson <cshored@thecshore.com>
2019-08-17 00:37:34 -0400

    travis ci tests: Add tests and CI (Travis)
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

---
+++ ci/travis-cmd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#./tests/scripts/prepare_test_environment
+./tests/scripts/run_all_tests
---
+++ tests/scripts/t01000-tidy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# hugo-oldnew-mashup test framework
+#
+#    Copyright (C) 2019 Daniel F. Dickinson <cshored@thecshore.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+OTS=SUCCESS
+
+htfiles="$(find public -name '*.html' -print)"
+
+for htfile in $htfiles; do
+	if ! tidy -o /dev/null -q --drop-empty-elements no --indent auto $htfile; then
+		echo "FAIL: Linting (tidy) fails for $htfile"
+		exit 3
+	else
+		echo "PASS: Linting (tidy) succeeds for $htfile"
+	fi
+done
+
+exit 0
